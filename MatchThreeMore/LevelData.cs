27862b353ac862e8b646961c1601ea99ce97ca38
artem.koloskov <artem.koloskov@LIVE.RU>
2019-02-25 16:30:22 +1000

    first

---
+++ MatchThreeMore/LevelData.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+
+namespace MatchThreeMore
+{
+    public class LevelData
+    {
+        public string[,] level;
+        
+        public static LevelData LoadFrom (string fileName)
+        {
+            string data = File.ReadAllText(fileName);
+
+            LevelData levelData = Newtonsoft.Json.JsonConvert.DeserializeObject<LevelData>(data);
+
+            return levelData;
+        }
+
+        public (GemType, bool, bool) GetGemTypeFromLevelDataAt (int row, int column)
+        {
+            switch (level[row, column])
+            {
+                case "T":
+                    return (GemType.triangle, true, false);
+                case "S":
+                    return (GemType.square, true, false);
+                case "D":
+                    return (GemType.diamond, true, false);
+                case "P":
+                    return (GemType.pentagon, true, false);
+                case "H":
+                    return (GemType.hexagon, true, false);
+                case "T'":
+                    return (GemType.triangle, true, true);
+                case "S'":
+                    return (GemType.square, true, true);
+                case "D'":
+                    return (GemType.diamond, true, true);
+                case "P'":
+                    return (GemType.pentagon, true, true);
+                case "H'":
+                    return (GemType.hexagon, true, true);
+                case "t":
+                    return (GemType.triangle, false, false);
+                case "s":
+                    return (GemType.square, false, false);
+                case "d":
+                    return (GemType.diamond, false, false);
+                case "p":
+                    return (GemType.pentagon, false, false);
+                case "h":
+                    return (GemType.hexagon, false, false);
+                default:
+                    return (GemType.triangle, false, false);
+            }
+
+
+        }
+    }
+}
---
+++ MatchThreeMore/Resources/:Users:artem.koloskov:Projects:MatchThreeMore:MatchThreeMore:MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using Foundation;
+using System;
+using UIKit;
+using GameKit;
+
+namespace MatchThreeMore
+{
+    public partial class MainMenu : UIViewController
+    {
+        public MainMenu (IntPtr handle) : base (handle)
+        {
+            
+        }
+
+
+    }
+}
